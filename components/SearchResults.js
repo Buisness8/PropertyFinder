75b9817eb424a74c5775a6e2bcde610bcfe45113
Chris Ritchie <chrisritchi@gmail.com>
2016-10-27 17:46:08 -0500

    styled search results

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -11,6 +11,34 @@ const {
   Text
 } = ReactNative;
 
+var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
+
 class SearchResults extends Component{
 	constructor(props){
 		super(props);
@@ -21,15 +49,28 @@ class SearchResults extends Component{
 		};
 	}
 
-	renderRow(rowData, sectionID, rowID){
-		return(
-			<TouchableHighlight
-				underlayColor='#dddddd'>
-				<View>
-					<Text>{rowData.title}</Text>
-				</View>
-			</TouchableHighlight>
-		);
+	renderRow(rowData, sectionID, rowID) {
+	  var price = rowData.price_formatted.split(' ')[0];
+	 
+	  return (
+	    <TouchableHighlight onPress={() => this.rowPressed(rowData.lister_url)}
+	        underlayColor='#dddddd'>
+	      <View>
+	        <View style={styles.rowContainer}>
+	          <Image style={styles.thumb} source={{ uri: rowData.img_url }} />
+	          <View  style={styles.textContainer}>
+	            <Text style={styles.price}>{price}</Text>
+	            <Text style={styles.title}
+	                  numberOfLines={1}>{rowData.title}</Text>
+	          </View>
+	        </View>
+	        <View style={styles.separator}/>
+	      </View>
+	    </TouchableHighlight>
+	  );
+	}
+	rowPressed(listerURL) {
+	  var property = this.props.listings.filter(prop => prop.lister_url === listerURL)[0];
 	}
 
 	render(){
@@ -41,4 +82,4 @@ class SearchResults extends Component{
 	}
 }
 
-module.exports =SearchResults;
+module.exports = SearchResults;
